10189ef8eb5a1d9377a23dd399561aa8cbaf3649
Supportprojektfokus <support@projektfokus.ch>
2025-05-01 08:55:03 +0200

    entrypoint änderung für laufstart redmine

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
-bundle install
+# Alte PID-Datei entfernen
+rm -f tmp/pids/server.pid
+
+# Abhängigkeiten ohne Entwicklung und Test installieren
+bundle install --without development test
+
+# Datenbank-Migrationen ausführen
 bundle exec rake db:migrate RAILS_ENV=production
 
-exec rails server -b 0.0.0.0 -e production
+# Puma-Server explizit starten
+exec rails server -u puma -b 0.0.0.0 -e production
